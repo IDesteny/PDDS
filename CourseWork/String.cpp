fdf0c03a2126610113eed2342e004e391812c47e
Ilya <Ilya@DESKTOP-NGE4JD2>
2022-04-12 19:45:56 +0300

    del-<algorithm>

--- CourseWork/String.cpp
+++ CourseWork/String.cpp
@@ -1,1 +1,75 @@
 #include "String.hpp"
+
+String::String() = default;
+
+String::String(std::string s) : s(s) {}
+
+String::String(int n, int min, int max)
+{
+	s.reserve(n);
+
+	for (auto i = 0; i < n; ++i)
+	{
+		char ch;
+		while (own(ch = min + min + rand() % (max - min)));
+		s += ch;
+	}
+}
+
+bool String::own(char ch)
+{
+	return s.find(ch) != std::string::npos;
+}
+
+int String::size()
+{
+	return s.size();
+}
+
+bool String::subset(const String & _s) const
+{
+	return _s.s.find(s);
+}
+
+bool String::quality(const String & _s)
+{
+	return _s.subset(*this) && subset(_s);
+}
+
+String String::union_set(const String & _s)
+{
+	String new_string(s);
+
+	for (auto &&iter : _s.s)
+		if (own(iter))
+			new_string.s += iter;
+
+	return new_string;
+}
+
+String String::intersection_set(const String & _s)
+{
+	String new_string;
+
+	for (auto &&iter : _s.s)
+		if (own(iter))
+			new_string.s += iter;
+
+	return new_string;
+}
+
+String String::difference_set(const String & _s)
+{
+	String new_string;
+
+	for (auto &&iter : _s.s)
+		if (!own(iter))
+			new_string.s += iter;
+
+	return new_string;
+}
+
+String String::symmetric_difference_set(const String & _s)
+{
+	return intersection_set(_s).difference_set(union_set(_s));
+}
--- CourseWork/String.hpp
+++ CourseWork/String.hpp
@@ -1,85 +1,20 @@
 #pragma once
 #include <string>
-#include <algorithm>
-#include <iterator>
-#include <iostream>
 
 class String
 {
 	std::string s;
 
 public:
-	String() = default;
-
-	String(std::string s) : s(s) {}
-
-	String(int n, int min, int max)
-	{
-		s.reserve(n);
-
-		for (auto i = 0; i < n; ++i)
-		{
-			char ch;
-			while (own(ch = min + min + rand() % (max - min)));
-			s += ch;
-		}
-	}
-
-	bool own(char ch)
-	{
-		return s.find(ch) != std::string::npos;
-	}
-
-	int size()
-	{
-		return s.size();
-	}
-
-	bool subset(const String &_s)
-	{
-		return _s.s.find(s);
-	}
-
-	bool quality(const String &_s)
-	{
-		return std::equal(s.begin(), s.end(), _s.s.begin());
-	}
-
-	String union_set(const String &_s)
-	{
-		String new_string(s);
-
-		for (auto &&iter : _s.s)
-			if (own(iter))
-				new_string.s += iter;
-
-		return new_string;
-	}
-
-	String intersection_set(const String &_s)
-	{
-		String new_string;
-
-		for (auto &&iter : _s.s)
-			if (own(iter))
-				new_string.s += iter;
-
-		return new_string;
-	}
-
-	String difference_set(const String &_s)
-	{
-		String new_string;
-
-		for (auto &&iter : _s.s)
-			if (!own(iter))
-				new_string.s += iter;
-
-		return new_string;
-	}
-
-	String symmetric_difference_set(const String &_s)
-	{
-		return intersection_set(_s).difference_set(union_set(_s));
-	}
+	String();
+	String(std::string s);
+	String(int n, int min, int max);
+	bool own(char ch);
+	int size();
+	bool subset(const String &_s) const;
+	bool quality(const String &_s);
+	String union_set(const String &_s);
+	String intersection_set(const String &_s);
+	String difference_set(const String &_s);
+	String symmetric_difference_set(const String &_s);
 };
