4456f673a080d1f6ef6f79f14030e7a5e7a90ef9
Ilya <Ilya@DESKTOP-NGE4JD2>
2022-05-30 16:26:52 +0300

    refact

--- CourseWork/String.cpp
+++ CourseWork/String.cpp
@@ -4,8 +4,7 @@ String::String() = default;
 
 String::String(const int n, const int mn, const int mx) noexcept
 {
-	for (auto &&i : std::ranges::views::iota(0, n))
-		while (!_add(mn + mn + rand() % (mx - mn)));
+	std::ranges::for_each(std::ranges::views::iota(0, n), [&](auto &&i) { while (!_add(mn + mn + rand() % (mx - mn))); });
 }
 
 bool String::_add(auto &&v) noexcept
@@ -36,22 +35,19 @@ bool String::quality(const String &_s) const noexcept
 String String::union_set(const String &_s) const noexcept
 {
 	auto ns(_s);
-	std::ranges::for_each(_s.s, [&](auto &&v) { ns._add(v); });
-	return ns;
+	return std::ranges::for_each(_s.s, [&](auto &&v) { ns._add(v); }), ns;
 }
 
 String String::intersection_set(const String &_s) const noexcept
 {
 	String ns;
-	std::ranges::for_each(_s.s, [&](auto &&v) { ns._add(v); });
-	return ns;
+	return std::ranges::for_each(_s.s, [&](auto &&v) { ns._add(v); }), ns;
 }
 
 String String::difference_set(const String &_s) const noexcept
 {
 	auto ns(_s);
-	std::ranges::for_each(_s.s, [&](auto &&v) { !own(v) && ns._add(v); });
-	return ns;
+	return std::ranges::for_each(_s.s, [&](auto &&v) { !own(v) && ns._add(v); }), ns;
 }
 
 String String::symmetric_difference_set(const String &_s) const noexcept
